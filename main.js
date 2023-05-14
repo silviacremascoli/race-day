abf8ecd749dca9d58d4fd175553bdb91b4e722a3
silviacremascoli <silcremascoli@gmail.com>
2023-05-14 15:21:43 +0200

    Added if statements

--- main.js
+++ main.js
@@ -2,3 +2,13 @@ let raceNumber = Math.floor(Math.random() * 1000);
 
 let earlyRegister = true;
 let runnerAge = 25;
+
+if (runnerAge > 18 && earlyRegister) {
+  raceNumber = raceNumber + 1000;
+}
+
+if (runnerAge > 18 && earlyRegister) {
+  console.log(`Starting time for your number (${raceNumber}) is 9:30 am.`);
+} else if (runnerAge > 18 && !earlyRegister) {
+  console.log(`Starting time for your number (${raceNumber}) is 11:00 am.`);
+}
