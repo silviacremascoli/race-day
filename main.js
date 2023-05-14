0984169463595ce5e0694d8650ac403b740066cf
silviacremascoli <silcremascoli@gmail.com>
2023-05-14 15:31:58 +0200

    Added further age ranges

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 let raceNumber = Math.floor(Math.random() * 1000);
 
 let earlyRegister = true;
-let runnerAge = 25;
+let runnerAge = 18;
 
 if (runnerAge > 18 && earlyRegister) {
   raceNumber = raceNumber + 1000;
@@ -11,4 +11,8 @@ if (runnerAge > 18 && earlyRegister) {
   console.log(`Starting time for your number (${raceNumber}) is 9:30 am.`);
 } else if (runnerAge > 18 && !earlyRegister) {
   console.log(`Starting time for your number (${raceNumber}) is 11:00 am.`);
+} else if (runnerAge < 18) {
+  console.log(`Starting time for your number (${raceNumber}) is 12:30 pm.`);
+} else {
+  console.log(`Please clarify with the contact desk.`);
 }
